8a8194fc7e2557669998097d9ad137cc537c5eed
Huzaifa181 <huzaifa.ahmed181@gmail.com>
2021-06-18 01:46:15 +0500

    Add the transaction history and summary component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import React, { useEffect } from "react";
 import {BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Header from './components/Header'
 import Balance from './components/Balance'
-
+import AccountSummary from './components/AccountSummary'
+import TransactionHistory from './components/TransactionHistory'
+import './App.css'
 function App() {
   return (
     <div className='app'>
         <Header/>
         <div className="container">
           <Balance/>
+          <AccountSummary/>
+          <TransactionHistory/>
         </div>
     </div>
   );
